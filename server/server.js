c254fb5355ca7d3b9b06ee14a8e228b5bb8efc1d
jValdez <jdv_rodriguez@hotmail.com>
2020-09-18 15:04:40 -0500

    Configuracion de la db

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 require('./config/config')
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 
@@ -13,7 +14,7 @@ app.use(bodyParser.json())
 
 // Usuarios
 app.get('/usuarios', function (req, res) {
-  res.json('get Usuario')
+    res.json('get Usuario')
 });
 
 app.post('/usuarios', function (req, res) {
@@ -21,20 +22,31 @@ app.post('/usuarios', function (req, res) {
     res.json({
         persona,
     })
-  });
+});
 
-  app.put('/usuarios/:id', function (req, res) {
-      let id = req.params.id;
+app.put('/usuarios/:id', function (req, res) {
+    let id = req.params.id;
     res.json({
         id,
 
     })
-  });
+});
 
-  app.delete('/usuarios', function (req, res) {
+app.delete('/usuarios', function (req, res) {
     res.json('delete Usuario')
-  });
+});
 
-app.listen(process.env.PORT, () =>{
+app.listen(process.env.PORT, () => {
     console.log('Escuchando en: ', process.env.PORT);
 })
+
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then((res)=>{
+    console.log('Base de datos online');
+}).catch((err)=>{
+console.log(err);
+});
